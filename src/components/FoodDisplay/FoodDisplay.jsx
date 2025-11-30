84cc3dc96cf2af5124590e5eac7e83ce8c7c1542
kiruthikaannadurai01 <kiruthikaa732@gamil.com>
2025-11-30 21:40:59 +0530

    your message here

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,8 +8,13 @@ export const FoodDisplay = ({category}) => {
   return (
     <div className = 'food-display' id = 'food-display'>
       <h2> Top dishes near you </h2>
+      <div className = "food - display-list">
+        {food_list.map((food_item)=>{ 
+          return<Fooditem key ={index} id ={item._id} name = {item.name} price={item.price} discription={item.discription} image={item.image}/>
+})}
+      </div>
     </div>
   )
-}
+}   
 export default FoodDisplay
 
---
+++ src/components/Fooditem/Fooditem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Fooditem.css'
+export const Fooditem = ({id,name,price,discription,image}) => {
+  return (
+    <div>Fooditem</div>
+  )
+}
+export default Fooditem
