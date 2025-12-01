9e9ff22175b6539e7d7a024fc377400c4ac992ed
kiruthikaannadurai01 <kiruthikaa732@gamil.com>
2025-12-01 22:32:49 +0530

    your message

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,20 +1,34 @@
 import React, { useContext } from "react";
+import "./FoodDisplay.css";
 
-import './FoodDisplay.css'
-import { StoreContext } from '../context/StoreContext'
-export const FoodDisplay = ({category}) => {
-  const{food_list} = useContext(StoreContext)
+import { StoreContext } from "../context/StoreContext";
+import FoodItem from "../Fooditem/Fooditem";
+
+export const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
 
   return (
-    <div className = 'food-display' id = 'food-display'>
-      <h2> Top dishes near you </h2>
-      <div className = "food - display-list">
-        {food_list.map((food_item)=>{ 
-          return<Fooditem key ={index} id ={item._id} name = {item.name} price={item.price} discription={item.discription} image={item.image}/>
-})}
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+
+      <div className="food-display-list">
+        {food_list
+          .filter((item) =>
+            category === "All" ? true : item.category === category
+          )
+          .map((food_item) => (
+            <FoodItem
+              key={food_item._id}
+              id={food_item._id}
+              name={food_item.name}
+              description={food_item.description}
+              price={food_item.price}
+              image={food_item.image}
+            />
+          ))}
       </div>
     </div>
-  )
-}   
-export default FoodDisplay
+  );
+};
 
+export default FoodDisplay;
--- src/components/Fooditem/Fooditem.jsx
+++ src/components/Fooditem/Fooditem.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import './Fooditem.css'
-export const Fooditem = ({id,name,price,discription,image}) => {
+import React from "react";
+import "./Fooditem.css";
+import { assets } from "../../assets/assets";
+
+function FoodItem({ id, name, price, description, image }) {
   return (
-    <div>Fooditem</div>
-  )
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt={name} />
+      </div>
+
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+        <img src={assets.rating_starts} alt="rating" />
+
+        </div>
+
+        <p className="food-item-description">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
 }
-export default Fooditem
+
+export default FoodItem;
